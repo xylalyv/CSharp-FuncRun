45195c135bc5f63e1ab7c12bc00195ecc34f4cd5
xeyal.ramile15@gmail.com <xeyal.ramile15@gmail.com>
2023-05-15 10:11:46 +0400

    Commit

---
+++ FuncRun/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace FuncRun
+{
+    public delegate void Func(string str);
+    public class MyClass
+    {
+        public void Space(string str)
+        {
+            string spaced_string = string.Join("_", str.ToCharArray());
+            Console.WriteLine(spaced_string);
+        }
+
+        public void Reverse(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed_string = new string(charArray);
+            Console.WriteLine(reversed_string);
+        }
+    }
+
+    public class Run
+    {
+        public void runFunc(Func funcDel, string str)
+        {
+            funcDel(str);
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string:");
+            string? str = Console.ReadLine();
+
+            MyClass cls = new MyClass();
+
+            Func funcDel = null;
+            funcDel += cls.Space;
+            funcDel += cls.Reverse;
+
+            Run run = new Run();
+            run.runFunc(funcDel, str);
+        }
+    }
+ 
+
+
+
+
+
+
+}
